20ab6f32cd2c8a34d36736a80f475406b9a31bcc
Bruno Patrick <81782238+Bruno-Patrick@users.noreply.github.com>
2022-03-26 08:52:32 -0500

    Views Criadas

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -4,7 +4,8 @@ from django.views.generic import TemplateView
 from .views import *
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html'), name='index' ),
     path('', CriarProjeto.as_view(), name='cadastro-de-projetos' ),
     path('listagem/', ProjetosListagem.as_view(), name='listagem-de-projetos' ),
+    path('update/<int:pk>', EditarProjeto.as_view() , name='editar-projeto'),
+    path('deletar/<int:pk>', DeletarProjeto.as_view() , name='deletar-projeto'),
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,19 +1,27 @@
 from dataclasses import field
 from urllib import request
-from django.shortcuts import redirect, render
-from django.views.generic import CreateView, DeleteView, ListView
+from django.shortcuts import redirect
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from .forms import *
-#from .models import ProjetosModel
-
-#Redirecionamento da página
 from django.urls import reverse_lazy
 
 class CriarProjeto(CreateView):
   model = ProjetosModel
   fields = '__all__'
   template_name="index.html"
-  sucess_url = reverse_lazy('index.html')
+  sucess_url = reverse_lazy('listagem-de-projetos')
 
 class ProjetosListagem(ListView):
   model = ProjetosModel
   template_name = 'app/listar_cadastros.html'
+
+class EditarProjeto(UpdateView):
+  model = ProjetosModel
+  fields = "__all__" 
+  template_name = 'app/index_cadastros.html'
+  success_url = reverse_lazy('listagem-de-projetos')
+
+class DeletarProjeto(DeleteView):
+    model = ProjetosModel
+    template_name = 'app/excluir_cadastros.html'
+    success_url = reverse_lazy('listagem-de-projetos')

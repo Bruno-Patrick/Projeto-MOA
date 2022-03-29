f71fab2ff9b64cc1b4d8f02348447bd50a9e75bc
Bruno-Patrick <021996.bmx@gmail.com>
2022-03-29 16:43:41 -0500

    Drag And Drop Funcions OK

---
+++ static/js/drag-and-drop.js
@@ -0,0 +1,50 @@
+const cards = document.querySelectorAll('.draggcard')
+const dropzone = document.querySelectorAll('.dropzone')
+
+cards.forEach(draggcard => {
+    draggcard.addEventListener('dragstart', dragstart)
+    draggcard.addEventListener('drag', drag)
+    draggcard.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+    dropzone.forEach( dropzone => dropzone.classList.add('highlight'))
+
+    this.classList.add('is-dragging')
+}
+
+function drag() {
+}
+
+function dragend() {
+    dropzone.forEach( dropzone => dropzone.classList.remove('highlight'))
+    this.classList.remove('is-dragging')
+}
+
+// Dropzone
+
+dropzone.forEach( dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+}
+
+function dragover() {
+    this.classList.add('over')
+
+    const cardBeingDragged = document.querySelector('.is-dragging')
+
+    this.appendChild(cardBeingDragged)
+}
+
+function dragleave() {
+    this.classList.remove('over')
+}
+
+function drop() {
+    this.classList.remove('over')
+}
